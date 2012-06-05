38ff7eabde6712ce7204b3c50ad6150a432e60b0
Amir Malik <a@unoc.net>
2012-06-04 23:21:56 -0700

    Add static file server with a basic middleware stack.

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log('TODO: implement server');
+/**
+ * server.js
+ * UserSpy server
+ */
+
+var express = require('express'),
+         io = require('socket.io');
+
+var app = express.createServer();
+
+app.configure(function() {
+  // log all requests
+  app.use(express.logger('dev'));
+  
+  // serve static files out of public dir for matching URIs
+  app.use(express.static(__dirname + '/public'));
+  
+  // parse query strings into the req.query object
+  app.use(express.query());
+  
+  // setup routes
+  app.use(express.router(router));
+  
+  // dump errors to the browser
+  app.use(express.errorHandler({showStack: true}));
+});
+
+app.listen(3000, function() {
+  console.log('UserSpy server started.');
+});
+
+function router(app) {
+  app.get('/hello', function(req, res) {
+    res.write('Hello, ' + req.query['name'] + '!');
+    res.end();
+  });
+}
