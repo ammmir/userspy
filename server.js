503462547a208c0470ce59dce6ad3555f86fc2a1
Amir Malik <a@unoc.net>
2012-06-06 21:30:23 -0700

    Add server-side functionality for rough demo.

--- server.js
+++ server.js
@@ -25,13 +25,70 @@ app.configure(function() {
   app.use(express.errorHandler({showStack: true}));
 });
 
+// bind Socket.IO to the server
+io = io.listen(app);
+
+io.configure(function() {
+  io.set('log level', '1');
+});
+
 app.listen(3000, function() {
   console.log('UserSpy server started.');
 });
+
+io.sockets.on('connection', on_connection);
 
 function router(app) {
   app.get('/hello', function(req, res) {
     res.write('Hello, ' + req.query['name'] + '!');
     res.end();
+  });
+
+  app.get('/api/visitors', function(req, res) {
+    var page_url = req.query['url'];
+
+    res.json(VISITORS[page_url]);
+    res.end();
+  });
+}
+
+var VISITORS = {}; // url -> socket.id -> { page info ... }
+var SOCKET_URL = {}; // socket.id -> url
+
+function on_connection(socket) {
+  socket.on('page info', function(info) {
+    VISITORS[info.url] = VISITORS[info.url] || (VISITORS[info.url] = {});
+    VISITORS[info.url][socket.id] = info;
+    SOCKET_URL[socket.id] = info.url;
+    
+    socket.emit('ok');
+  });
+  
+  socket.on('events', function(events) {
+    var page_url = SOCKET_URL[socket.id];
+    
+    if(!page_url)
+      return;
+    
+    socket.broadcast.to(page_url + '#' + socket.id).emit('events', events);
+  });
+  
+  socket.on('spy', function(spy_params) {
+    var current_users = VISITORS[spy_params.url] || {};
+    
+    if(!current_users[spy_params.socket_id])
+      return socket.emit('error', 'no such socket_id connected');
+    
+    var page_info = VISITORS[spy_params.url][spy_params.socket_id];
+    
+    socket.emit('page info', page_info);
+    socket.join(spy_params.url + '#' + spy_params.socket_id);
+  });
+  
+  socket.on('disconnect', function() {
+    for(var page_url in VISITORS) {
+      if(socket.id in VISITORS[page_url])
+        delete VISITORS[page_url][socket.id];
+    }
   });
 }
